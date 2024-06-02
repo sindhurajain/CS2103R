178634d5831caa70eea418e0b485452bff586203
sindhurajain <sindhura394@gmail.com>
2024-06-02 19:56:08 +0800

    Site Update. [skip ci]

---
+++ contents/features.page-vue-render.js
@@ -0,0 +1,43 @@
+
+    var pageVueRenderFn = function anonymous(
+) {
+with(this){return _c('div',{attrs:{"id":"app"}},[_c('header',{attrs:{"sticky":""}},[_c('navbar',{attrs:{"type":"dark"},scopedSlots:_u([{key:"brand",fn:function(){return [_c('a',{staticClass:"navbar-brand",attrs:{"href":"/CS2103R/index.html","title":"Home"}},[_v("GitHub Project Boards for tP")])]},proxy:true},{key:"right",fn:function(){return [_c('li',[_c('form',{staticClass:"navbar-form"},[_c('searchbar',{attrs:{"data":searchData,"placeholder":"Search","on-hit":searchCallback,"menu-align-right":""}})],1)])]},proxy:true}])},[_v(" "),_c('li',[_c('a',{staticClass:"nav-link",attrs:{"href":"/CS2103R/index.html"}},[_v("Home")])]),_v(" "),_c('li',[_c('a',{staticClass:"nav-link",attrs:{"href":"/CS2103R/contents/setup.html"}},[_v("Set Up & Add Tasks")])]),_v(" "),_c('li',[_c('a',{staticClass:"nav-link",attrs:{"href":"/CS2103R/contents/features.html"}},[_v("Features")])]),_v(" "),_c('dropdown',{staticClass:"nav-link",scopedSlots:_u([{key:"header",fn:function(){return [_v("Topic 3")]},proxy:true}])},[_v(" "),_c('li',[_c('a',{staticClass:"dropdown-item",attrs:{"href":"/CS2103R/contents/topic3a.html"}},[_v("Topic 3a")])]),_v(" "),_c('li',[_c('a',{staticClass:"dropdown-item",attrs:{"href":"/CS2103R/contents/topic3b.html"}},[_v("Topic 3b")])])])],1)],1),_v(" "),_c('div',{attrs:{"id":"flex-body"}},[_c('overlay-source',{attrs:{"id":"site-nav","tag-name":"nav","to":"site-nav"}},[_c('div',{staticClass:"site-nav-top"},[_c('div',{staticClass:"fw-bold mb-2",staticStyle:{"font-size":"1.25rem"}},[_v("Contents")])]),_v(" "),_c('div',{staticClass:"nav-component slim-scroll"},[_c('site-nav',[_c('overlay-source',{staticClass:"site-nav-list site-nav-list-root",attrs:{"tag-name":"ul","to":"mb-site-nav"}},[_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-0",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/CS2103R/index.html"}},[_v("Home")])])]),_v(" "),_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-0",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/CS2103R/contents/setup.html"}},[_v("Set Up & Add Tasks")])])]),_v(" "),_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-0",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/CS2103R/contents/features.html"}},[_v("Features")])])]),_v(" "),_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-0",attrs:{"onclick":"handleSiteNavClick(this)"}},[_v("Topic 3 \n\n"),_c('div',{staticClass:"site-nav-dropdown-btn-container"},[_c('i',{staticClass:"site-nav-dropdown-btn-icon site-nav-rotate-icon",attrs:{"onclick":"handleSiteNavClick(this.parentNode.parentNode, false); event.stopPropagation();"}},[_c('span',{staticClass:"glyphicon glyphicon-menu-down",attrs:{"aria-hidden":"true"}})])])]),_c('ul',{staticClass:"site-nav-dropdown-container site-nav-dropdown-container-open site-nav-list"},[_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-1",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/CS2103R/contents/topic3a.html"}},[_v("Topic 3a")])])]),_v(" "),_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-1",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/CS2103R/contents/topic3b.html"}},[_v("Topic 3b")])])])])])])],1)],1)]),_v(" "),_c('div',{attrs:{"id":"content-wrapper"}},[_c('breadcrumb'),_v(" "),_c('br'),_v(" "),_m(0),_v(" "),_m(1),_v(" "),_m(2),_v(" "),_c('div',{staticStyle:{"page-break-after":"always"}}),_v(" "),_c('hr'),_v(" "),_m(3),_v(" "),_m(4),_v(" "),_m(5),_v(" "),_m(6),_v(" "),_m(7),_v(" "),_m(8),_v(" "),_m(9),_v(" "),_m(10)],1),_v(" "),_c('overlay-source',{attrs:{"id":"page-nav","tag-name":"nav","to":"page-nav"}},[_c('div',{staticClass:"nav-component slim-scroll"})]),_v(" "),_c('scroll-top-button')],1),_v(" "),_m(11)])}
+};
+    var pageVueStaticRenderFns = [function anonymous(
+) {
+with(this){return _c('h1',{attrs:{"id":"features"}},[_v("Features"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#features","onclick":"event.stopPropagation()"}})])}
+},function anonymous(
+) {
+with(this){return _c('h2',{attrs:{"id":"table-of-contents"}},[_v("Table of Contents"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#table-of-contents","onclick":"event.stopPropagation()"}})])}
+},function anonymous(
+) {
+with(this){return _c('ul',[_c('li',[_c('a',{attrs:{"href":"#useful-features"}},[_v("Useful Features")]),_v(" "),_c('ul',[_c('li',[_c('a',{attrs:{"href":"#adding-fields-to-tasks"}},[_v("Adding Fields to Tasks")])]),_v(" "),_c('li',[_c('a',{attrs:{"href":"#visualisation-of-tasks-in-different-formats"}},[_v("Visualisation of Tasks in Different Formats")])]),_v(" "),_c('li',[_c('a',{attrs:{"href":"#automated-insights"}},[_v("Automated Insights")])]),_v(" "),_c('li',[_c('a',{attrs:{"href":"#creating-automated-workflows"}},[_v("Creating Automated Workflows")])])])]),_v(" "),_c('li',[_c('a',{attrs:{"href":"#faq"}},[_v("FAQ")])]),_v(" "),_c('li',[_c('a',{attrs:{"href":"#glossary"}},[_v("Glossary")])])])}
+},function anonymous(
+) {
+with(this){return _c('h2',{attrs:{"id":"useful-features"}},[_v("Useful Features"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#useful-features","onclick":"event.stopPropagation()"}})])}
+},function anonymous(
+) {
+with(this){return _c('h5',{attrs:{"id":"adding-fields-to-tasks"}},[_v("Adding Fields to Tasks"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#adding-fields-to-tasks","onclick":"event.stopPropagation()"}})])}
+},function anonymous(
+) {
+with(this){return _c('h5',{attrs:{"id":"visualisation-of-tasks-in-different-formats"}},[_v("Visualisation of Tasks in Different Formats"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#visualisation-of-tasks-in-different-formats","onclick":"event.stopPropagation()"}})])}
+},function anonymous(
+) {
+with(this){return _c('h5',{attrs:{"id":"automated-insights"}},[_v("Automated Insights"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#automated-insights","onclick":"event.stopPropagation()"}})])}
+},function anonymous(
+) {
+with(this){return _c('h5',{attrs:{"id":"creating-automated-workflows"}},[_v("Creating Automated Workflows"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#creating-automated-workflows","onclick":"event.stopPropagation()"}})])}
+},function anonymous(
+) {
+with(this){return _c('h2',{attrs:{"id":"faq"}},[_v("FAQ"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#faq","onclick":"event.stopPropagation()"}})])}
+},function anonymous(
+) {
+with(this){return _c('p',[_c('strong',[_v("Q")]),_v("\nA")])}
+},function anonymous(
+) {
+with(this){return _c('h2',{attrs:{"id":"glossary"}},[_v("Glossary"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#glossary","onclick":"event.stopPropagation()"}})])}
+},function anonymous(
+) {
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("[Generated by "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.5.2")]),_v("]")])])])}
+}];
+  
